5020b7beb9dde7af529884e1a9fdde3ebabf07b9
Jorge Francisco <Jorge7.9@hotmail.com>
2022-05-28 16:28:14 -0300

    refactor: exportando dados do componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
-  declarations: [AppComponent, NovaTransferenciaComponent],
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
